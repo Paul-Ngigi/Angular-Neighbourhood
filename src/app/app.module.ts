0f379b5a17e7b6408ae54603cfe71bdf4d194eb1
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-07 13:11:13 +0300

    add navbar, business and neighbourhood components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NeighbourhoodComponent } from './components/neighbourhood/neighbourhood.component';
+import { BusinessComponent } from './components/business/business.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NeighbourhoodComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
