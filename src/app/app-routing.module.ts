9db679075386545f385c7b5645e28d5a48da1c4c
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-08 00:20:33 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './components/business/business.component';
+import { NeighbourhoodComponent } from './components/neighbourhood/neighbourhood.component';
+import { AddBusinessComponent } from './components/add-business/add-business.component';
+import { AddNeighbourhoodComponent } from './components/add-neighbourhood/add-neighbourhood.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'businesses', component: BusinessComponent},
+  {path: 'add-business', component: AddBusinessComponent},
+  {path: 'neighbourhood', component: NeighbourhoodComponent},
+  {path: 'add-neighbourhood', component: AddNeighbourhoodComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
