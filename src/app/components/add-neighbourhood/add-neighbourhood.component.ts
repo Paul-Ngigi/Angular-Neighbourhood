13b889bfafa124d63c8dee91db4a3985ba86c426
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-08 10:37:16 +0300

    add create component form

--- src/app/components/add-neighbourhood/add-neighbourhood.component.ts
+++ src/app/components/add-neighbourhood/add-neighbourhood.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NeighbourhoodService } from 'src/app/service/neighbourhood/neighbourhood.service';
+import { Neighbourhood } from '../../interfaces/neighbourhood'
 
 @Component({
   selector: 'app-add-neighbourhood',
   templateUrl: './add-neighbourhood.component.html',
   styleUrls: ['./add-neighbourhood.component.css']
 })
-export class AddNeighbourhoodComponent implements OnInit {
+export class AddNeighbourhoodComponent {
 
-  constructor() { }
+  neighbourhood?:Neighbourhood[]=[];
 
+  log(x:any) {console.log(x);}
+  constructor(private neighbourhoodservice: NeighbourhoodService) { }
+
+  // create 
+  onSubmit(neighbourhood:any){
+    this.neighbourhoodservice.create(neighbourhood).subscribe((result) =>{
+      console.warn("result",result)
+    })
+    console.warn(neighbourhood)
+  }
   ngOnInit(): void {
   }
 
